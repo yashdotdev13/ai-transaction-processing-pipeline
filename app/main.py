f92cff16c581d1c5fa8935907132cbff004cb359
98001yash <yashchauhan.gaya@gmail.com>
2026-07-01 09:45:36 +0530

    healh check is done and working

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from fastapi import FastAPI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from app.api.routes.health import router as health_router
+from app.core.config import settings
 
+app = FastAPI(
+    title=settings.APP_NAME,
+    version="1.0.0",
+)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.include_router(health_router)
